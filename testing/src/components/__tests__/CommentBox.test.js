665c56ecab06868b54800f6a0568a0d54bc353db
Gonzalo Bonora <gebonora@gmail.com>
2021-01-06 14:11:50 -0300

    Testing with events and props

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -15,3 +15,21 @@ it("has a text are and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
+
+it("has a textarea where users can type in", () => {
+  wrapped
+    .find("textarea")
+    .simulate("change", { target: { value: "new comment" } });
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("empties the value of the textarea after submit", () => {
+  wrapped
+    .find("textarea")
+    .simulate("change", { target: { value: "new comment" } });
+  wrapped.update();
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
+});
