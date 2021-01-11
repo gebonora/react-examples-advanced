b800855decdd024194c174b60ca972b140ecc6a5
Gonzalo Bonora <gebonora@gmail.com>
2021-01-11 13:29:18 -0300

    Checking if signed in

--- hocs/src/components/CommentBox.js
+++ hocs/src/components/CommentBox.js
@@ -5,6 +5,20 @@ import * as actions from "actions";
 class CommentBox extends Component {
   state = { comment: "" };
 
+  componentDidMount() {
+    this.shouldNavigateAway();
+  }
+
+  componentDidUpdate() {
+    this.shouldNavigateAway();
+  }
+
+  shouldNavigateAway() {
+    if (!this.props.auth) {
+      console.log("I need to leave");
+    }
+  }
+
   handleChange = (event) => {
     this.setState({ comment: event.target.value });
   };
@@ -33,4 +47,10 @@ class CommentBox extends Component {
   }
 }
 
-export default connect(null, actions)(CommentBox);
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  };
+};
+
+export default connect(mapStateToProps, actions)(CommentBox);
--- hocs/src/reducers/index.js
+++ hocs/src/reducers/index.js
@@ -1,4 +1,8 @@
 import { combineReducers } from "redux";
 import commentsReducer from "reducers/comments";
+import authReducer from "reducers/auth";
 
-export default combineReducers({ comments: commentsReducer });
+export default combineReducers({
+  comments: commentsReducer,
+  auth: authReducer,
+});
