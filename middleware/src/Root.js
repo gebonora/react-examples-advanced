a21bc3d550269abfe9c37b61dbd06b195e9b55a7
Gonzalo Bonora <gebonora@gmail.com>
2021-01-11 16:45:00 -0300

    Middleware

--- middleware/src/Root.js
+++ middleware/src/Root.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
-import reduxPromise from "redux-promise";
+
 import reducers from "reducers";
+import async from "middlewares/async";
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(
-    reducers,
-    initialState,
-    applyMiddleware(reduxPromise)
-  );
+  const store = createStore(reducers, initialState, applyMiddleware(async));
   return <Provider store={store}>{children}</Provider>;
 };
