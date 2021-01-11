98f79839bf894cc24d56f94629893ce8305c7655
Gonzalo Bonora <gebonora@gmail.com>
2021-01-11 18:02:55 -0300

    State validation middleware

--- middleware/src/Root.js
+++ middleware/src/Root.js
@@ -4,8 +4,13 @@ import { applyMiddleware, createStore } from "redux";
 
 import reducers from "reducers";
 import async from "middlewares/async";
+import stateValidator from "middlewares/stateValidator";
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(async, stateValidator)
+  );
   return <Provider store={store}>{children}</Provider>;
 };
