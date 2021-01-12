43a1fe04081ae357b244d6a4ec03a018d1fbf98a
Gonzalo Bonora <gebonora@gmail.com>
2021-01-12 14:40:47 -0300

    Controllers and authentication flow

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require("./controllers/authentication");
+
 module.exports = function (app) {
-  app.get("/", function (req, res, next) {
-    res.send(["hi", "bye", "hello"]);
-  });
+  app.post("/signup", Authentication.signup);
 };
