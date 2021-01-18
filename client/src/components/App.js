62ea58440410574b4e1edc192a733e319a7ff714
Gonzalo Bonora <gebonora@gmail.com>
2021-01-18 15:21:20 -0300

    Added Header and Welcome message. Using React router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Header from "components/Header";
 
-export default () => {
-  return <div>App</div>;
+export default ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 };
